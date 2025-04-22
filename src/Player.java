536299fef4c50874c357696add40ac7e36da3cfa
filip <fiflok1810@gmail.com>
2025-04-22 14:27:21 +0200

    more

--- src/Player.java
+++ src/Player.java
@@ -27,41 +27,68 @@ public class Player {
         this.playerHeight = height;
     }
 
-    public void update(List<Platform> platforms, int panelHeight, int panelWidth){
+    public void update(List<Platform> platforms, int panelHeight, int panelWidth) {
+        int prevX = positionX;
+        int prevY = positionY;
 
-        if (pressedKeys.contains(KeyEvent.VK_LEFT)||pressedKeys.contains(KeyEvent.VK_A)) {
+        // Ruch poziomy
+        if (pressedKeys.contains(KeyEvent.VK_LEFT) || pressedKeys.contains(KeyEvent.VK_A)) {
             positionX -= speed;
         }
-        if (pressedKeys.contains(KeyEvent.VK_RIGHT)||pressedKeys.contains(KeyEvent.VK_D)) {
+        if (pressedKeys.contains(KeyEvent.VK_RIGHT) || pressedKeys.contains(KeyEvent.VK_D)) {
             positionX += speed;
         }
-        if ((pressedKeys.contains(KeyEvent.VK_W )|| pressedKeys.contains((KeyEvent.VK_SPACE))) && onGround) {
+
+        // Grawitacja i skok
+        if ((pressedKeys.contains(KeyEvent.VK_W) || pressedKeys.contains(KeyEvent.VK_SPACE)) && onGround) {
             velocityY = -10;
             onGround = false;
         }
-        // GRAWITACJA
-        if (!onGround) {
-            velocityY += gravity;
-            if (velocityY > maxFallSpeed) {
-                velocityY = maxFallSpeed;
+        velocityY += gravity;
+        if (velocityY > maxFallSpeed) velocityY = maxFallSpeed;
+        positionY += velocityY;
+
+        Rectangle playerBounds = new Rectangle(positionX, positionY, playerWidth, playerHeight);
+        onGround = false;
+
+        for (Platform platform : platforms) {
+            Rectangle platBounds = platform.getBounds();
+            if (playerBounds.intersects(platBounds)) {
+                Rectangle intersection = playerBounds.intersection(platBounds);
+
+                // Sprawdzamy kierunek kolizji
+                if (prevY + playerHeight <= platBounds.y) {
+                    // Kolizja od góry (lądowanie)
+                    positionY = platBounds.y - playerHeight;
+                    velocityY = 0;
+                    onGround = true;
+                } else if (prevY >= platBounds.y + platBounds.height) {
+                    // Kolizja od dołu (sufit)
+                    positionY = platBounds.y + platBounds.height;
+                    velocityY = 0;
+                } else if (prevX + playerWidth <= platBounds.x) {
+                    // Kolizja z lewej strony
+                    positionX = platBounds.x - playerWidth;
+                } else if (prevX >= platBounds.x + platBounds.width) {
+                    // Kolizja z prawej strony
+                    positionX = platBounds.x + platBounds.width;
+                }
+
+                // Aktualizuj playerBounds po poprawkach
+                playerBounds = new Rectangle(positionX, positionY, playerWidth, playerHeight);
             }
         }
 
-        positionY +=  velocityY;
-
-        // "Podłoga" – zatrzymaj spadanie
+        // Ograniczenia ekranu
         if (positionY + playerHeight >= panelHeight) {
             positionY = panelHeight - playerHeight;
             velocityY = 0;
             onGround = true;
-        } else {
-            onGround = false;
         }
-        if(positionX +playerWidth >= panelWidth){
-            positionX = panelWidth - playerWidth;
-
-        }
+        if (positionX < 0) positionX = 0;
+        if (positionX + playerWidth > panelWidth) positionX = panelWidth - playerWidth;
     }
+
 
 
     protected void draw(Graphics g) {
